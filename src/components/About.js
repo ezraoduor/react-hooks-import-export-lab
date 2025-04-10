4d68bb09c6ead2d0c30416a7c1283f7452799491
Ezra Oduor <ezrachurchill20@gmail.com>
2025-04-10 20:37:51 +0300

    imported and exported components

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,15 @@
+// src/components/About.js
 import React from "react";
+
+const image = "https://i.imgur.com/mV8PQxj.gif";
 
 function About() {
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>I Made This</p>
+      <h2>About</h2>
       <img src={image} alt="I made this" />
     </div>
   );
 }
+
+export default About;
